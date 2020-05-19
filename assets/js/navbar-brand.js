65508b2f7b630cea6e9e02f2442cdb0180e7ac1c
climateactiondarwin <climateactiondarwintech@gmail.com>
2020-05-19 23:40:26 +0930

    request from Charlie, make navbar brand not an active link if on Home

--- assets/js/navbar-brand.js
+++ assets/js/navbar-brand.js
@@ -1,9 +1,9 @@
 
 $(".collapse").on("hidden.bs.collapse", function() {
-    $("a.navbar-brand").removeClass("cad-nav-menu-open");
+    $(".navbar-brand").removeClass("cad-nav-menu-open");
 });
 
 $(".collapse").on("show.bs.collapse", function() {
-    $("a.navbar-brand").addClass("cad-nav-menu-open");
+    $(".navbar-brand").addClass("cad-nav-menu-open");
 });
 
